529a2c7c70373ac1756392a2dcf9b1f6bd4038ff
Hamza129 <ithamza189@gmail.com>
2021-10-11 00:30:05 +0500

    Add project files.

---
+++ PointOfSale/Controllers/PosController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PointOfSale.Controllers
+{
+    public class PosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
